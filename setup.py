72509ef51e52536c4b099029c86756acd440ba57
Willem Jan Faber <wjfprive@xs4all.nl>
2017-02-28 13:18:36 +0100

    Working buildout?

--- setup.py
+++ setup.py
@@ -1,2 +1,41 @@
-#insert setup here
-adafruit-ws2801
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'test':
+    os.system('python test_requests.py')
+    sys.exit()
+
+entry_point = ("""
+        [console_scripts]
+        noise=Fe2.tools.alsa:noise
+        mute=Fe2.tools.alsa:mute
+""")
+
+required = ['adafruit-gpio',
+            'adafruit-ws2801',
+            'json',
+            'pyinotify',
+            '']
+setup(
+    name='bollocks',
+    version=1,
+    description=' Controll multicolor leds strips from SHM ',
+    email='Willem Jan Faber <aloha@pruts.nl> Henrie Aanstoot <fash@pruts.nl>',
+    author='Henrie Aanstoot, Willem Jan Faber',
+    url='https://github.com/WillemJan/bollocks/',
+    #scripts=['./statusscripts', './example'],
+    package_dir={'bollocks': '.'},
+    packages=['bollocks', 'bollocks'],
+    install_requires=required,
+    license='GPL-3.0',
+    entry_points=entry_point,
+)
+
