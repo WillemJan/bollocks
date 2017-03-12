a996b13a150ae46f7e810d449ff8883354c24823
Willem Jan Faber <wjfprive@xs4all.nl>
2017-03-12 18:32:45 +0100

    Expicitly copy pep8 and virtualenv to chrooted env

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -7,8 +7,8 @@ VERSION=wheezy
 CHROOT_ARCH=armhf
 
 # Debian package dependencies for the host
-HOST_DEPENDENCIES="debootstrap qemu-user-static binfmt-support sbuild"
-
+HOST_DEPENDENCIES="debootstrap qemu-user-static binfmt-support sbuild python-virtualenv pep8 "
+-
 # Debian package dependencies for the chrooted environment
 GUEST_DEPENDENCIES="build-essential git m4 sudo python python-virtualenv pep8"
 
@@ -24,6 +24,11 @@ function setup_arm_chroot {
     sudo debootstrap --foreign --no-check-gpg --include=fakeroot,build-essential \
         --arch=${CHROOT_ARCH} ${VERSION} ${CHROOT_DIR} ${MIRROR}
     sudo cp /usr/bin/qemu-arm-static ${CHROOT_DIR}/usr/bin/
+
+    # PEP8 && virtualenv
+    sudo cp /usr/bin/pep8 ${CHROOT_DIR}/usr/bin/
+    sudo cp /usr/bin/virtualenv ${CHROOT_DIR}/usr/bin/
+
     sudo chroot ${CHROOT_DIR} ./debootstrap/debootstrap --second-stage
     sudo sbuild-createchroot --arch=${CHROOT_ARCH} --foreign --setup-only \
         ${VERSION} ${CHROOT_DIR} ${MIRROR}
